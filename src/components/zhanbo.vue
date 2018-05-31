fb668367f26557949bfd81d98e9d124fd0b61cd9
fire821126 <76469312@qq.com>
2018-06-01 07:10:01 +0800

    zhanbu

--- src/components/zhanbo.vue
+++ src/components/zhanbo.vue
@@ -1,5 +1,5 @@
 <template>
-        <div>
+    <div class="m-zhanbu">
         zhanbo
     </div>
 </template>
@@ -11,5 +11,5 @@ export default {
 </script>
 
 <style>
-
+.m-zhanbu{ width:100%;height:100%; display:inline-block;background: url("../assets/images/bg-zb.png") no-repeat;background-size: 100% 100%;}
 </style>
