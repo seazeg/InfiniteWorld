e9d2cfd1ab11ce1582c009ba9bf04e9579343c63
周尔耕 <zhou.ergeng@trs.com.cn>
2018-06-03 18:39:44 +0800

    update

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,12 +1,79 @@
 <template>
   <div>
-    {{$store.state.show}}
+    <div class="login">
+      <input type="password" value="" v-model="password">
+      <p>
+        <input type="checkbox" name="" id="">
+        <span>记住秘钥</span>
+        <a>创建秘钥</a>
+      </p>
+      <div class="box">
+        <img src="../assets/images/login_button.png" alt="" class="submit" @click="submit()">
+      </div>
+      <div style="text-align: center;position: fixed;bottom: 1rem;">
+        <img src="../assets/images/login_logo.png" alt="" style="width:70%">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        password: ""
+      }
+    },
+    methods: {
+      submit() {
 
+      }
+    }
   }
 
 </script>
+
+<style>
+  .login {
+    width: 96%;
+    margin-left: 2%;
+    height: 380px;
+    background: url("../assets/images/login_bg.png") no-repeat;
+    background-size: 100%;
+    position: absolute;
+    top: 20%;
+  }
+
+  .login input[type=password] {
+    width: 56%;
+    margin: 2.5rem 3.1rem 0 2.8rem;
+    position: relative;
+    z-index: 10;
+    background: transparent;
+    padding: 10px;
+    font-size: 0.4rem;
+    outline: 0;
+    border: 0;
+    color: #eebc7f;
+  }
+
+  .login .box {
+    text-align: center;
+    margin-top: 1rem;
+  }
+
+  .login>p {
+    font-size: 0.4rem;
+    color: #6a1802;
+    width: 50%;
+    margin: 0 auto;
+    margin-top: 0.5rem;
+  }
+
+  .login .box img {
+    width: 70%;
+  }
+
+
+
+</style>
