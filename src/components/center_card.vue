d2348957e2db2c748a9a2b646995631e1ccacd4b
fire821126 <76469312@qq.com>
2018-06-08 07:02:27 +0800

    卡牌列表详情

--- src/components/center_card.vue
+++ src/components/center_card.vue
@@ -23,11 +23,11 @@
 	<div v-show="tcShow" class="m-tcbg"></div>
     <div v-if="tcShow" class="m-tc">
 		<div class="m-xqcard"><img src="../assets/images/card01.png" /></div>
-		<div class="m-cardxqbtn"><img src="../assets/images/ico-xqbtn.png" /></div>
-		<div>
-			<div class="m-txt01">当前产出：26843</div>
-			<div class="m-txt02">领悟需求：26843</div>
-			<div class="m-txt03">上次产出时间：<br />2018/5/3 14:11</div>
+		<div v-if="!cardxq" class="m-cardxqbtn" @click="openCard()"><img src="../assets/images/ico-xqbtn.png" /></div>
+		<div v-if="cardxq" class="m-xqtxtbox" @click="openCard()">
+			<div class="m-txt01">当前产出：<span>26843</span></div>
+			<div class="m-txt02">领悟需求：<span>26843</span></div>
+			<div class="m-txt03">上次产出时间：<span>2018/5/3 14:11</span></div>
 		</div>
 	</div>
 
@@ -39,11 +39,15 @@ export default {
 	    data() {
       return {
         tcShow: false,
+		cardxq: false,
       }
     },
     methods: {
       open() {
         this.tcShow = !this.tcShow
+      },
+	  openCard() {
+        this.cardxq = !this.cardxq
       },
     }
 
@@ -76,15 +80,6 @@ export default {
 	.center-card .m-list .m-imgbox .m-tc{
 		width: 4rem;height: 5rem;display: inline-block;left: 0; top:0;position: absolute;z-index: 100;
 	}
-	.center-card .m-list .m-imgbox .m-tc .m-txt01{
-		width: 100%; display: inline-block; height: .75rem;background: rgba(71,44,14,.66); text-align: center;color: #fff;font-size: .36rem;line-height: .75rem;vertical-align: top; margin-top: .3rem;
-	}
-	.center-card .m-list .m-imgbox .m-tc .m-txt02{
-		width: 100%; display: inline-block; height: .75rem;background: rgba(71,44,14,.66); text-align: center;color: #fff;font-size: .36rem;line-height: .75rem;vertical-align: top; margin-top: .3rem;
-	}
-	.center-card .m-list .m-imgbox .m-tc .m-txt03{
-		width: 100%; display: inline-block; padding:.3rem 0;background: rgba(71,44,14,.66); text-align: center;color: #fff;font-size: .36rem;line-height: .5rem;vertical-align: top; margin-top: .3rem;
-	}
 	.center-card .m-tc {
     width: 9.35rem;
     height: 12.23rem;
@@ -113,6 +108,21 @@ export default {
 		width: 100%;
     	height: 100%;
     	vertical-align: top;
+	}
+	.center-card .m-tc .m-xqtxtbox{
+	  width: 6.3rem;height: 9.3rem;display: inline-block;position: absolute;left:1.52rem;top:1rem;background:rgba(0,0,0, .8);z-index: 1000;
+  }
+	.center-card .m-tc .m-xqtxtbox .m-txt01{
+		width: 80%; display: inline-block;  padding:.3rem 0;text-align: left;color: #fff;font-size: .48rem;line-height: .7rem;vertical-align: top; margin-top: .3rem;
+	}
+	.center-card .m-tc .m-xqtxtbox .m-txt02{
+		width: 80%; display: inline-block;  padding:.3rem 0; text-align: left;color: #fff;font-size: .48rem;line-height: .7rem;vertical-align: top; margin-top: .3rem;
+	}
+	.center-card .m-tc .m-xqtxtbox .m-txt03{
+		width: 80%; display: inline-block; padding:.3rem 0; text-align: left;color: #fff;font-size: .48rem;line-height: .7rem;vertical-align: top; margin-top: .3rem;
+	}
+	.center-card .m-tc .m-xqtxtbox .m-txt01 span, .center-card .m-tc .m-xqtxtbox .m-txt02 span, .center-card .m-tc .m-xqtxtbox .m-txt03 span{
+		width: 100%;display: inline-block; padding-left: .4rem; box-sizing: border-box;
 	}
 
 </style>
